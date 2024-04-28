696feacf85976b05e6b59df9b6f5ef1b15ce1963
adarsh-gupta101 <adarshgupta2468@gmail.com>
2024-04-28 18:18:26 +0530

    updates

--- src/components/getResults.tsx
+++ src/components/getResults.tsx
@@ -9,7 +9,7 @@ export default async function getResultFromTavily(userInput: string) {
    await fetchDataFromTavily(userInput).then((retrievedDocs:any) => {
      console.log({ retrievedDocs });
 
-     PrePareDataasync(retrievedDocs as any); // Replace arg2 and arg3 with the actual arguments
+     PrePareDataasync(retrievedDocs as any,false); // Replace arg2 and arg3 with the actual arguments
         //   PrePareDataasync(retrievedDocs as any); // Replace arg2 and arg3 with the actual arguments
      // return retrievedDocs;
    })
